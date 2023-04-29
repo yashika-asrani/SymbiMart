7aea1dc6fee1dc9bf6a0b73e6f9fc230f1b526da
yashika-asrani <yashiikkaa7@gmail.com>
2023-04-29 12:22:58 +0530

    Added new files for frontend and backend

---
+++ backend/models/UserSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+const User = mongoose.model("user", UserSchema);
+User.createIndexes();
+module.exports = User;
